802716209e9a54bcc7f222f1975b96b50bddb88d
Alex_Sandler <troubadour1488@gmail.com>
2025-01-12 08:19:41 +0300

    Adding and structuring a component for the Slogan block.

---
+++ src/layout/sections/slogan/Slogan.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import {SectionTitle} from "../../../components/SectionTitle";
+import {Button} from "../../../components/Button";
+
+export const Slogan = () => {
+    return (
+        <StyledSlogan>
+            <SectionTitle>I Am Available For Freelance</SectionTitle>
+            <Button>Hire me</Button>
+        </StyledSlogan>
+    );
+};
+
+const StyledSlogan = styled.section`
+  min-height: 30vh;
+  background-color: #d997ec;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+`
